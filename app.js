ae9c6ad9b3cdfa9e47b85cea2d2a97cc427de04c
MilosGak97 <milosgak2020@gmail.com>
2024-07-16 04:23:54 -0400

    Initial commit

--- app.js
+++ app.js
@@ -24,32 +24,8 @@ connectDB()
 // Middleware to parse JSON bodies with a size limit of 1MB
 app.use(bodyParser.json({ limit: '1mb' }));
 
-// Middleware to handle gzip decompression
-app.use((req, res, next) => {
-    if (req.headers['content-encoding'] === 'gzip') {
-        let buf = Buffer.from('');
-        req.on('data', (chunk) => {
-            buf = Buffer.concat([buf, chunk]);
-        });
-        req.on('end', () => {
-            zlib.gunzip(buf, (err, decoded) => {
-                if (err) {
-                    console.error('Error decoding gzip content:', err);
-                    return res.status(400).send('Bad Request: Error decoding gzip content');
-                }
-                try {
-                    req.body = JSON.parse(decoded.toString());
-                    next();
-                } catch (error) {
-                    console.error('Error parsing JSON:', error);
-                    return res.status(400).send('Bad Request: Error parsing JSON');
-                }
-            });
-        });
-    } else {
-        next();
-    }
-});
+// Use gzip decompression middleware
+app.use(zlibMiddleware);
 
 // Middleware to log incoming requests
 app.use((req, res, next) => {
---
+++ src/middleware/zlib.js
@@ -0,0 +1,30 @@
+// src/middleware/gzipMiddleware.js
+const zlib = require('zlib');
+
+const gzipMiddleware = (req, res, next) => {
+    if (req.headers['content-encoding'] === 'gzip') {
+        let buf = Buffer.from('');
+        req.on('data', (chunk) => {
+            buf = Buffer.concat([buf, chunk]);
+        });
+        req.on('end', () => {
+            zlib.gunzip(buf, (err, decoded) => {
+                if (err) {
+                    console.error('Error decoding gzip content:', err);
+                    return res.status(400).send('Bad Request: Error decoding gzip content');
+                }
+                try {
+                    req.body = JSON.parse(decoded.toString());
+                    next();
+                } catch (error) {
+                    console.error('Error parsing JSON:', error);
+                    return res.status(400).send('Bad Request: Error parsing JSON');
+                }
+            });
+        });
+    } else {
+        next();
+    }
+};
+
+module.exports = gzipMiddleware;
