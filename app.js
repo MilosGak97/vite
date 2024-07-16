a4688d725087449d82cb524afaa55c215fff58aa
MilosGak97 <milosgak2020@gmail.com>
2024-07-16 02:56:12 -0400

    Initial commit

--- app.js
+++ app.js
@@ -20,17 +20,32 @@ client.connect(err => {
     }
     db = client.db('propertyListings');
     console.log('Connected to MongoDB');
+
+    // Start the server after successful DB connection
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
 });
 
 // Middleware to parse JSON bodies with a size limit of 1MB
 app.use(bodyParser.json({ limit: '1mb' }));
 
+// Middleware to log incoming requests
+app.use((req, res, next) => {
+    console.log('Incoming request headers:', req.headers);
+    next();
+});
+
 // Webhook endpoint
 app.post('/webhook', async (req, res) => {
     try {
-        const dataArray = req.body; // Assuming req.body is an array of objects
-        console.log("DATA ARRAY: ", dataArray);
+        console.log('Request body:', req.body);
 
+        if (!Array.isArray(req.body)) {
+            throw new Error('Request body is not an array');
+        }
+
+        const dataArray = req.body; // Assuming req.body is an array of objects
         const collection = db.collection('properties'); // Use your collection name
 
         // Save all properties to MongoDB
@@ -40,11 +55,6 @@ app.post('/webhook', async (req, res) => {
         res.status(200).send('Webhook received successfully');
     } catch (error) {
         console.error('Failed to handle webhook:', error);
-        res.status(400).send('Bad Request: Invalid JSON');
+        res.status(400).send(`Bad Request: ${error.message}`);
     }
-});
-
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
 });
