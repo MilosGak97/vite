0dd3e2f4f7246c0a9ffd5cd46f7e554674b28f6a
MilosGak97 <milosgak2020@gmail.com>
2024-07-17 15:59:49 -0400

    Initial commit

--- server.js
+++ server.js
@@ -16,6 +16,28 @@ app.use('/', webhookHandler);
 // Endpoint to trigger sendPostRequests
 app.post('/trigger', sendPostRequests);
 
+app.get('/pull', (req, res) => {
+    async function fetchData() {
+        const snapshotId = 's_lyq9dteux7tpuvxui';
+        const accessToken = 'a3a53d23-02a3-4b70-93b6-09cd3eda8f39';
+        const url = `https://api.brightdata.com/datasets/v3/snapshot/${snapshotId}?format=json`;
+
+        try {
+            const response = await axios.get(url, {
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`
+                }
+            });
+
+            console.log('Response dataYEEEEEY:', response.data);
+            return response.data;
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            throw error; // or handle gracefully
+        }
+    }
+    fetchData();
+})
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
