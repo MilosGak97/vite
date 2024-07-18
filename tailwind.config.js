aee744d7be5a74b8bb627a6e87058a81e15e89b6
MilosGak97 <milosgak2020@gmail.com>
2024-07-18 11:15:41 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+module.exports = {
+  content: [
+    './src/public/**/*.html',
+    './src/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
