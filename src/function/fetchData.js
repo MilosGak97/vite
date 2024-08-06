518ec7cb304be6bd0157149d81538fc2946500be
MilosGak97 <milosgak2020@gmail.com>
2024-08-06 06:18:14 -0400

    Initial commit

--- src/function/fetchData.js
+++ src/function/fetchData.js
@@ -6,33 +6,32 @@ async function fetchData(snapshotId) {
     const accessToken = 'a3a53d23-02a3-4b70-93b6-09cd3eda8f39';
     const url2 = `https://api.brightdata.com/datasets/v3/snapshot/${snapshotId}?format=json`;
 
-    while (true) {
-        try {
-            const response = await axios.get(url2, {
-                headers: {
-                    'Authorization': `Bearer ${accessToken}`
-                },
-                maxContentLength: Infinity,
-                maxBodyLength: Infinity
-            });
+    try {
+        const response = await axios.get(url2, {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            },
+            maxContentLength: Infinity,
+            maxBodyLength: Infinity
+        });
 
-            //console.log("THIS RESPONSE:", response.data);
-            if (Array.isArray(response.data)) {
-                console.log("Response is an array with length:", response.data.length);
-            } else {
-                console.log("Response is not an array");
-            }
-            console.log("Type of response.data:", typeof response.data);
-
-            const responseSize = JSON.stringify(response.data).length;
-            console.log("Size of JSON response in bytes:", responseSize);
-
-
-        } catch (error) {
-            console.error('Error fetching data:', error);
-            throw error; // or handle gracefully
+        //console.log("THIS RESPONSE:", response.data);
+        if (Array.isArray(response.data)) {
+            console.log("Response is an array with length:", response.data.length);
+        } else {
+            console.log("Response is not an array");
         }
+        console.log("Type of response.data:", typeof response.data);
+
+        const responseSize = JSON.stringify(response.data).length;
+        console.log("Size of JSON response in bytes:", responseSize);
+
+
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error; // or handle gracefully
     }
+
 }
 
 module.exports = { fetchData };
