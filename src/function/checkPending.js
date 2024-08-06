9f5a77e6d516a025b0fd35652dc85ff3f93a0add
MilosGak97 <milosgak2020@gmail.com>
2024-08-06 08:51:02 -0400

    Initial commit

--- src/function/checkPending.js
+++ src/function/checkPending.js
@@ -3,14 +3,15 @@ const { client } = require('../config/mongodb');
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 async function checkPending(data, status_check_snapshot_id) {
-    if (Array.isArray(data)) {
+    if (Array.isArray(data, status_check_snapshot_id)) {
         const dataArray = data;
         const collection = client.db().collection('properties');
 
         for (let i = 0; i < dataArray.length; i++) {
             const listing = dataArray[i];
             const { zpid, hdpTypeDimension, contingent_listing_type } = listing;
-
+            let status_check_snapshot_id;
+            console.log(status_check_snapshot_id);
             try {
                 const exists = await checkIfZpidExists(zpid);
                 if (exists) {
