875f1c02d1a114d2730101751dce6ff69a330244
MilosGak97 <milosgak2020@gmail.com>
2024-08-06 14:29:49 -0400

    Initial commit

--- Procfile
+++ Procfile
@@ -1,6 +1,2 @@
 web: npm start
 worker: node worker.js
-worker: node worker2.js
-worker: node worker3.js
-worker: node worker4.js
-worker: node worker5.js
--- worker.js
+++ worker.js
@@ -5,11 +5,7 @@ const { connectDB } = require('./src/config/mongodb');
 async function processPendingChecks() {
     // Example: Fetching a list of snapshot IDs to process
     const snapshotIds = [
-        's_lzigept0t1mx52y9h',
-        's_lzigews51npvdtkn96',
-        's_lzigf4fw1ezrms5fl6',
-        's_lzigfd0n2bckfmc6nl',
-        's_lzigfl82mih7qmggt'
+        's_lzigao042o93jb11ou'
     ];
 
     for (const snapshotId of snapshotIds) {
